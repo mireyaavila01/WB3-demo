633990e2880446851ca6303ab814b9baafd541a4
Mireya Avila <mireyaavila147@yahoo.com>
2024-05-03 11:27:12 -0400

    created a demo

---
+++ WebDemo1/scripts/index.js
@@ -0,0 +1,25 @@
+"use strict"
+
+
+window.onload = init;
+
+function init(){
+    const helloBtn= document.getElementById("helloBtn");
+    const goodbyeBtn = document.getElementById("goodbyeBtn");
+
+    helloBtn.onclick = onHelloBtnClick;
+    goodbyeBtn.onclick = onGoodbyeBtnClick;
+}
+
+
+function onHelloBtnClick(){
+    const output = document.getElementById("output");
+    output.innerHTML = "HELLO!"
+
+}
+
+function onGoodbyeBtnClick(){
+    const output = document.getElementById("output");
+    output.innerHTML = "GOODBYE!"
+
+}
